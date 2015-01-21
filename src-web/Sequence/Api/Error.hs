a2697b3b023e6256c4db6e27a8edc69a10f0b932
jbfp <jakobp10@gmail.com>
2015-01-21 20:40:09 +0100

    Error responses should now be proper json objects.

--- src-web/Sequence/Api/Error.hs
+++ src-web/Sequence/Api/Error.hs
@@ -2,9 +2,16 @@
 
 module Sequence.Api.Error where
 
+import Data.Aeson hiding (json)
+import Data.String (IsString)
 import Data.Text.Lazy
 import Network.HTTP.Types
 import Web.Scotty.Trans
+
+data ErrorResponse = ErrorResponse String
+
+instance ToJSON ErrorResponse where
+    toJSON (ErrorResponse err) = object [ "errorMessage" .= err ] 
 
 data ErrorResult = BadRequest String
                  | Unauthorized
@@ -16,10 +23,13 @@ data ErrorResult = BadRequest String
 instance ScottyError ErrorResult where
     stringError = InternalServerError
     showError = pack . show
+    
+toJsonError :: Monad m => String -> ActionT ErrorResult m ()
+toJsonError = json . ErrorResponse
 
 handleErrorResult :: Monad m => ErrorResult -> ActionT ErrorResult m ()
-handleErrorResult (BadRequest err)          = do status status400; json err
+handleErrorResult (BadRequest err)          = do status status400; toJsonError err
 handleErrorResult Unauthorized              = status status401
-handleErrorResult (UnauthorizedMessage err) = do status status401; json err
+handleErrorResult (UnauthorizedMessage err) = do status status401; toJsonError err
 handleErrorResult  NotFound                 = status status404
-handleErrorResult (InternalServerError err) = do status status500; json err
+handleErrorResult (InternalServerError err) = do status status500; toJsonError err
